7ec66a5dc0b446c990e81429ad9b7e85019d385b
Ahmedwagdymohy <ahmedwagdymohy@gmail.com>
2024-02-19 20:02:43 +0200

    Creating_KEYBAD_driver

--- MCU_interfacing/HAL/KEYPAD.c
+++ MCU_interfacing/HAL/KEYPAD.c
@@ -1,1 +1,97 @@
+/**
+ * @file KEYPAD.c
+ * @author AhmedWagdy 
+ * @brief 
+ * @version 0.1
+ * @date 2024-02-19
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
 
+
+#include "KEYBAD.h"
+
+
+
+
+
+/**
+ * @brief the Function checks for the pressed buttons and return the key as CHAR
+ *        for more explaination check the Connections file in Documentation "Digita_MultimeterProject\MCU_interfacing\Documents\Connections"
+ * @return char 
+ */
+
+char keyfind()
+{
+	while(1)
+	{
+	    KEY_DDR = 0xF0;           	/* set port direction as input-output */
+	    KEY_PRT = 0xFF;
+
+	    do
+	    {
+		KEY_PRT &= 0x0F;      		/* mask PORT for column read only */
+		asm("NOP");
+		colloc = (KEY_PIN & 0x0F); 	/* read status of column */
+	    }while(colloc != 0x0F);
+		
+	    do
+	    {
+		do
+		{
+	            _delay_ms(20);             /* 20ms key debounce time */
+		    colloc = (KEY_PIN & 0x0F);     /* read status of column */
+		}while(colloc == 0x0F);            /* check for any key press */
+			
+		_delay_ms (40);	                   /* 20 ms key debounce time */
+		colloc = (KEY_PIN & 0x0F);
+	    }while(colloc == 0x0F);
+
+	   /* now check for rows */
+	    KEY_PRT = 0xEF;            /* check for pressed key in 1st row */
+	    asm("NOP");
+	    colloc = (KEY_PIN & 0x0F);
+	    if(colloc != 0x0F)
+            {
+		rowloc = 0;
+		break;
+	    }
+
+	    KEY_PRT = 0xDF;		/* check for pressed key in 2nd row */
+	    asm("NOP");
+	    colloc = (KEY_PIN & 0x0F);
+	    if(colloc != 0x0F)
+	    {
+		rowloc = 1;
+		break;
+	    }
+		
+	    KEY_PRT = 0xBF;		/* check for pressed key in 3rd row */
+	    asm("NOP");
+            colloc = (KEY_PIN & 0x0F);
+	    if(colloc != 0x0F)
+	    {
+		rowloc = 2;
+		break;
+	    }
+
+	    KEY_PRT = 0x7F;		/* check for pressed key in 4th row */
+	    asm("NOP");
+	    colloc = (KEY_PIN & 0x0F);
+	    if(colloc != 0x0F)
+	    {
+		rowloc = 3;
+		break;
+	    }
+	}
+
+	if(colloc == 0x0E)
+	   return(keypad[rowloc][0]);
+	else if(colloc == 0x0D)
+	   return(keypad[rowloc][1]);
+	else if(colloc == 0x0B)
+	   return(keypad[rowloc][2]);
+	else
+	   return(keypad[rowloc][3]);
+}
--- MCU_interfacing/HAL/KEYPAD.h
+++ MCU_interfacing/HAL/KEYPAD.h
@@ -1,1 +1,44 @@
+/**
+ * @file KEYPAD.h
+ * @author AhmedWagdy
+ * @brief 
+ * @version 0.1
+ * @date 2024-02-19
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
 
+
+#ifndef KEYPAD_H_
+#define KEYPAD_H_
+
+
+
+#define KEY_PRT 	PORTA
+#define KEY_DDR		DDRA
+#define KEY_PIN		PINA
+
+
+
+
+unsigned char keypad[4][4] = {	{'7','8','9','/'},
+				                {'4','5','6','*'},
+			                	{'1','2','3','-'},
+			                	{' ','0','=','+'}};
+
+unsigned char colloc, rowloc;
+
+
+
+
+
+char keyfind();
+
+
+
+
+
+
+
+#endif
