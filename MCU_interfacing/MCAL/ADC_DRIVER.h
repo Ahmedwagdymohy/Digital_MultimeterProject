33e019a70930db7aa3ba9e7ffa188e639b4aa978
Ahmedwagdymohy <ahmedwagdymohy@gmail.com>
2024-02-26 04:24:37 +0200

    simple_error_edit

--- MCU_interfacing/MCAL/ADC_DRIVER.h
+++ MCU_interfacing/MCAL/ADC_DRIVER.h
@@ -2,19 +2,13 @@
  * ADC_DRIVER.h
  *
  * Created: 2/17/2024 1:41:57 AM
- * Author : Sakka
+ * Author : Sakka 
  */ 
 
 #ifndef ADC_DRIVER_H_
 #define ADC_DRIVER_H_
 
-<<<<<<< HEAD
-void    ();
-=======
-#include "../COTS/STD_TYPES.h"
-
 void ADC_INIT();
->>>>>>> 62f056d22a424a01349184dac00df9a8da6b6fa8
 
 u16 ADC_READ(u8 ch);
 
