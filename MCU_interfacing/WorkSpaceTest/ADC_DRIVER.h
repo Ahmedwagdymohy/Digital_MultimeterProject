bbc8373aa2e59e90093929efa3682e2504553b8b
Ahmed-Wagdy-Mohy <62253674+Ahmedwagdymohy@users.noreply.github.com>
2024-02-27 01:15:05 +0200

    Add files via upload

---
+++ MCU_interfacing/WorkSpaceTest/ADC_DRIVER.h
@@ -0,0 +1,15 @@
+/*
+ * ADC_DRIVER.h
+ *
+ * Created: 2/17/2024 1:41:57 AM
+ * Author : Sakka
+ */ 
+
+#ifndef ADC_DRIVER_H_
+#define ADC_DRIVER_H_
+
+void  ADC_INIT();
+
+u16 ADC_READ(u8 ch);
+
+#endif
